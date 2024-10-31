cb08ac05da9b8e0d5b69bc5b2ce7cb1418deed85
wafaa-muhamed <w.abdulrazzaq@wakeb.tech>
2024-10-31 13:18:34 +0300

    add main template and docker file

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include <bits/stdc++.h>
 int count;
+
+void even_counter() { count++; }
+
+void test() {
+  for (int i = 0; i < 50000; i++) {
+    even_counter();
+  }
+}
+int main() {
+  test();
+  std::cout << count << std::endl;
+}
